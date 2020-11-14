7a09417bdca0242128df60fbb1c333a770db9b55
marshanabilah <58138825+marshanabilah@users.noreply.github.com>
2020-11-14 20:11:52 +0700

    ubah view

--- Kuis2/Entities/Peminjam.cs
+++ Kuis2/Entities/Peminjam.cs
@@ -12,11 +12,11 @@ namespace Kuis2.Entities
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
 
     public partial class Peminjam
     {
-        [Required]
         [Display(Name = "ID Perpustakaan")]
         public int id { get; set; }
 
